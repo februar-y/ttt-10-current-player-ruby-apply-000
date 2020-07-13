1cc4dbd189447808f172f6f83c5f4b76c5f588cd
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-13 18:59:24 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
 def turn_count(board)
   turn = 0
   board.each do |position|
-    if position != " " or position != "" or position != nil
+    if position == "X" or position == "O"
       turn += 1
     end
-    turn
   end
+  turn
 end
 
 def current_player
